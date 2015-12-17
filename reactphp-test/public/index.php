2b897ed7e0789002d4bca1fb23e18864035d2e72
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-12-17 15:15:08 +0100

    Simple test with react js

---
+++ reactphp-test/public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$app = function ($request, $response) {
+    $response->writeHead(200, array('Content-Type' => 'text/plain'));
+    $params = $request->getQuery();
+
+    $time = 0;
+
+    if (isset($params['time'])) {
+        $time = $params['time'];
+    }
+
+    sleep($time);
+
+    $response->end("Sleept for $time");
+};
+
+$loop = React\EventLoop\Factory::create();
+$socket = new React\Socket\Server($loop);
+$http = new React\Http\Server($socket, $loop);
+
+$http->on('request', $app);
+echo "Server running at http://127.0.0.1:1337\n";
+
+$socket->listen(1337);
+$loop->run();
